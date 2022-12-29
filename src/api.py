f5753828efae4dbccb832d7f43f087e82cfe3b2b
Lakshya <lakshyam38@gmail.com>
2022-12-29 11:27:58 +0530

    ✨ Create endpoints and add CORS middleware

--- src/api.py
+++ src/api.py
@@ -1,8 +1,31 @@
 from fastapi import FastAPI
+from .spotify_handler import get_random_song
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://127.0.0.1",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
 def read_index():
     return {"msg": "OK"}
+
+
+@app.get("/random")
+def read_random_song():
+    return get_random_song()
